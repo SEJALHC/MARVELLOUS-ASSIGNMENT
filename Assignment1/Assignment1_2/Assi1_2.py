b95d7b9c59eb1d0e12406debc88c87b29fc37598
SEJALHC <chandaksejal58@gmail.com>
2025-09-22 11:05:56 +0530

    Add files via upload

---
+++ Assignment1/Assignment1_2/Assi1_2.py
@@ -0,0 +1,12 @@
+
+def ChkNum(number):
+    
+
+    if number %2==0:
+        print("number is even")
+    else:
+        print("number is odd")
+
+
+
+
---
+++ Assignment1/Assignment1_2/main.py
@@ -0,0 +1,6 @@
+from Assi1_2 import ChkNum
+
+
+number=int(input())
+result=ChkNum(number)
+print(f"{number}is{result}")
