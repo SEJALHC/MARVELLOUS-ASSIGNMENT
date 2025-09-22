2f04b02f9e2a970171e4ae249504b83b0231fde6
SEJALHC <chandaksejal58@gmail.com>
2025-09-22 11:04:43 +0530

    Add files via upload

---
+++ Assignment1/Assignment1_1/Assi1_1.py
@@ -0,0 +1,5 @@
+ #Write a program which contains one functio as fun().That function should display "Hello from fun" on console
+
+def Fun():
+    print("Hello from Fun") 
+
---
+++ Assignment1/Assignment1_1/main.py
@@ -0,0 +1,5 @@
+# Import the Fun function from the file where it is defined 
+from Assi1_1 import Fun
+
+# Call the function
+Fun()
